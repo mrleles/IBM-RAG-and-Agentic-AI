5eb06a4807b27cdf461a1845a59e38a68c5df7ac
Marlos Leles <marlosleles@gmail.com>
2025-11-10 11:31:45 -0300

    Semantic similarity with FAISS finished

--- 4 - Advanced RAG with Vector Databases and Retrievers/Semantic similarity with FAISS.py
+++ 4 - Advanced RAG with Vector Databases and Retrievers/Semantic similarity with FAISS.py
@@ -30,3 +30,34 @@ documents = newsgroups.data
 def preprocess_text(text):
     text = re.sub(r'^From:.*\n?', '', text, flags=re.MULTILINE)
     text = re.sub(r'\S*@\S?', '', text)
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = text.lower()
+    text = re.sub(r'\s+', '', text).strip()
+    return text
+
+processed_documents = [preprocess_text(doc) for doc in documents]
+
+print(preprocess_text(newsgroups_train.data[0])) # Print the preprocessed post
+
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+
+def embed_text(text):
+    return embed(text).numpy()
+
+X_use = np.vstack([embed_text([doc]) for doc in processed_documents])
+
+dimension = X_use.shape[1]
+index = faiss.IndexFlatL2(dimension)
+index.add(X_use)
+
+def search(query_text, k=5):
+    preprocessed_query = preprocess_text(query_text)
+    query_vector = embed_text([preprocessed_query])
+    distances, indices = index.search(query_vector.astype('float32'), k)
+    return distances, indices
+
+query_text = "motorcycle"
+distances, indices = search(query_text)
+
+for i, idx in enumerate(indices[0]):
+    print(f"Rank {i+1}: (Distance: {distances[0][i]})\n{processed_documents[idx]}\n")
