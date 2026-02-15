3dbde679b7159bd07caf1f5a3a283be60e3f88d6
Marlos Leles <marlosleles@gmail.com>
2026-02-15 10:55:42 -0300

    Data Visualization Agent.py finished

--- 6 - Building AI Agents/Data Visualization Agent.py
+++ 6 - Building AI Agents/Data Visualization Agent.py
@@ -21,3 +21,47 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 df = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/ZNoKMJ9rssJn-QbJ49kOzA/student-mat.csv")
+
+df.head(5)
+df.info()
+
+credentials = {
+	"url": "https://us-south.ml.cloud.ibm.com",
+}
+model_id = 'ibm/granite-3-2-8b-instruct'
+
+params = {
+	GenParams.MAX_NEW_TOKENS: 256,
+	GenParams.TEMPERATURE: 0,
+}
+
+project_id = "skills-network"
+
+space_id = None
+verify = False
+
+model = Model(
+	model_id=model_id,
+	credentials=credentials,
+	params=params,
+	project_id=project_id,
+	space_id=space_id,
+	verify=verify
+)
+
+llm = WatsonxLLM(model=model)
+
+agent = create_pandas_dataframe_agent(
+	llm, df, verbose=False,
+	return_intermediate_steps=True,
+	handle_parsing_errors="true"
+)
+
+response = agent.invoke("how many rows of data are in this file?")
+response['output']
+len(df)
+response['intermediate_steps'][-1][0].tool_input.replace('; ', '\n') # reveal the underlying commands
+
+response = agent.invoke("Give me all the data where student's age is over 18 years old.")
+response = agent.invoke("Generate a bar chart to plot the gender count.")
+response = agent.invoke("Generate a pie chart to display average value of Walc for each gender.")
