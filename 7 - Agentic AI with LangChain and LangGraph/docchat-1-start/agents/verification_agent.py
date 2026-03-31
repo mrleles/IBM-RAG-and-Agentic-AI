aae05c6559070030cdf51d9231baa980c35c7357
Marlos Leles <marlosleles@gmail.com>
2026-03-31 17:30:25 -0300

    verification_agent.py started

--- 7 - Agentic AI with LangChain and LangGraph/docchat-1-start/agents/verification_agent.py
+++ 7 - Agentic AI with LangChain and LangGraph/docchat-1-start/agents/verification_agent.py
@@ -11,5 +11,32 @@ client = APIClient(credentials)
 
 class VerificationAgent:
     def __init__(self):
-        # TODO
-        pass
+        """
+        Initialize the verification agent with the IBM WatsonX ModelInference.
+        """
+        # Initialize the WatsonX modelInference
+        print("Initializing VerificationAgent with IBM WatsonX ModelInference...")
+        self.model = ModelInference(
+            model_id="ibm/granite-4-h-small",
+            credentials=credentials,
+            project_id="skills-network",
+            params={
+                "max_token": 200,
+                "temperatura": 0.0,
+            }
+        )
+        print("ModelInference initialized successfully.")
+
+    def sanitize_response(self, response_text: str) -> str:
+        """
+        Satinize the LLM's response by stripping unnecessary whitespace.
+        """
+        return response_text.strip()
+
+    def generate_prompt(self, answer: str, context: str) -> str:
+        """
+        Generate a structured prompt for the LLM to verify the answer against the context.
+        """
+        prompt = f"""
+        you are an AI assistant designed to verify the accuracy and relevance of answers based on the provided context.
+        
