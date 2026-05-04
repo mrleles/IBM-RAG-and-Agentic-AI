c852cf4e3a498db59d9b9454541375963ddaff1e
Marlos Leles <marlosleles@gmail.com>
2026-05-04 17:36:09 -0300

    Orchestrations and Evaluation.py updated

--- 7 - Agentic AI with LangChain and LangGraph/LangGraph Design Patterns Orchestration & Evaluation.py
+++ 7 - Agentic AI with LangChain and LangGraph/LangGraph Design Patterns Orchestration & Evaluation.py
@@ -23,3 +23,64 @@ llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.7)
 
 import litellm
 litellm.ssl_verify = False
+
+# Dish schema for a single dish
+class Dish(BaseModel):
+    name: str = Field(
+        description="Name of the dish (for example, Spaghetti Bolognese, Chicken Curry)."
+    )
+    ingredients: List[str] = Field(
+        description="List of ingredients needed for this dish, separated by commas."
+    )
+    location: str = Field(
+        description="The cuisine or cultural origin of the dish (for example, Italian, Indian, Mexican)."
+    )
+
+# Dishes schema for a list of Dish objects
+class Dishes(BaseModel):
+    sections: List[Dish] = Field(
+        description="A list of grocery sections, one for each dish, with ingredients."
+    )
+
+# construct a prompt template
+dish_prompt = ChatPromptTemplate.from_messages([
+    (
+        "system",
+        "You are an assistant that generates a structured grocery list.\n\n"
+        "The user wants to prepare the following meals: {meals}\n\n"
+        "For each meal, return a section with:\n"
+        "- the name of the dish\n"
+        "- a comma-separated list of ingredients needed for that dish.\n"
+        "- the cuisine or cultural origin of the food"
+    )
+])
+
+planner_pipe = dish_prompt | llm.with_structured_output(Dishes)
+planner_pipe.invoke({"meals": ["banana smoothie", "carrot cake"]})
+
+class State(TypedDict):
+    meals: str
+    sections: List[Dish]
+    completed_menu: Annotated[List[str], operator.add]
+    final_meal_guide: str
+
+dummy_state: State = {
+    "meals": "Spaghetti Bolognese and Chicken Stir Fry",
+    "sections": [],
+    "completed_menu": [],
+    "final_meal_guide": ""
+}
+
+report_sections = planner_pipe.invoke({"meals": dummy_state['meals']})
+
+for i, section in enumerate(report_sections.sections):
+    print(f"Dish {i+1}\n")
+    dummy_state["sections"].append(section)
+    print(f"Item Name: {section.name}")
+    print(f"Location/Cuisine: {section.location}")
+    print(f"Ingredients: {section.ingredients}")
+
+def orchestrator(state: State):
+    """Orchestrator that generates a structured dish list from the given meals."""
+    dish_descriptions = planner_pipe.invoke({"meals": state["meals"]})
+    return {"sections": dish_descriptions}
