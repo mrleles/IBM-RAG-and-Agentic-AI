1ca99fe35708fa713d9c992ce2329eccf2fc9da9
Marlos Leles <marlosleles@gmail.com>
2026-06-14 11:15:41 -0300

    Stdio on Existing mcp servers.py done

--- 9 - AI Agents with MCP/Existing MCP Servers.py
+++ 9 - AI Agents with MCP/Existing MCP Servers.py
@@ -1,1 +1,41 @@
-# !pip
+# !pip install fastmcp
+
+sys.stdout.write("Hello")
+sys.exit("Some message")
+
+print("This is an error message", file=sys.stderr)
+
+from fastcpm import Client
+from fastmcp.client.transports import StreamableHttpTransport, StdioTransport
+
+# STDIO Transport
+stdio_transport = StdioTransport(
+    command = "npx",
+    args=["-y", "@upstash/context7-mcp"]
+)
+
+stdio_client = Client(stdio_transport)
+
+async with stdio_client as client:
+    tools = await client.list_tools()
+
+print(len(tools))
+print(tools[0].name)
+print(tools[0].description)
+print(tools[0].inputSchema)
+
+async with stdio_client as client:
+    response = await client.call_tool("resolve-library-id", {
+        "libraryName": "fastmcp",
+        "query": "I want to create a new MCP server using the fastmcp Python framework"
+    })
+
+print(response.content[0].text)
+
+async with stdio_client as client:
+    docs = await client.call_tool("query_docs", {
+        "libraryId": "/llmstxt/gofastmcp_llms-full_txt",
+        "query": "I want to fetch the code snippets and the documentation",
+        "tokens": 5000
+    })
+    print(docs.content[0].text[:1000])
